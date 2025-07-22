690aa95ac156d2d12db09bd342410e6739819037
Armen <armadamyan2009@gmail.com>
2025-07-22 21:35:47 +0400

    Added main

--- main.cpp
+++ main.cpp
@@ -156,28 +156,30 @@ public:
 
 int main()
 {
-    //Matrix mtxA( 11, 12 );
+    srand(time(0)); 
 
-    //mtxA.at( 3, 5 ) = 13;
-    //mtxA.at( 10, 11 ) = 36;
+    Matrix mtxA( 11, 12 );
 
-    //Matrix mtxB( 12, 11 );
+    mtxA.At( 3, 5 ) = 13;
+    mtxA.At( 10, 11 ) = 36;
+
+    Matrix mtxB( 12, 11 );
 
     // Should print an error and return an empty matrix,
     // as to add two matrices they should have the same row and column count
-    //MathOperation::Add( mtxA, mtxB );
+    MathOperation::Add( mtxA, mtxB );
 
     // Should succesfully do the multiplication
     // as to multiply two matrices the firstColCount should be equal to the secondRowCount
-    //Matrix mtxMult( MathOperation::Mult( mtxA, mtxB ) );
+    Matrix mtxMult( MathOperation::Mult( mtxA, mtxB ) );
 
-    //Printer::PrintToConsole( mtxMult );
+    Printer::PrintToConsole( mtxMult );
 
     // The matrix A should become from 11 x 12 to 12 x 11
-    //MathOperation::Transpose( mtxA );
+    MathOperation::Transpose( mtxA );
 
     // The addition is now possible as both matrixes have the same 12 x 11 size
-    //Printer::PrintToConsole( MathOperation::Add( mtxA, mtxB ) );
+    Printer::PrintToConsole( MathOperation::Add( mtxA, mtxB ) );
 
     return 0;
 }
